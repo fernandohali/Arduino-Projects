690662980acb323785317dfb78996dc73a510069
Fernando Hali <100550886+fernandohali@users.noreply.github.com>
2024-10-11 01:44:22 -0300

    Finalizado.

--- 7-Sensor-Ultrassonico-com-rotacao/7-Sensor-Ultrassonico-com-rotacao.ino
+++ 7-Sensor-Ultrassonico-com-rotacao/7-Sensor-Ultrassonico-com-rotacao.ino
@@ -1,53 +1,130 @@
-// Includes the Servo library
+// Inclui a biblioteca Servo
 #include <Servo.h>
 
-// Defines Tirg and Echo pins of the Ultrasonic Sensor
+// Define os pinos Trig e Echo do Sensor Ultrassônico
 const int trigPin = 10;
 const int echoPin = 9;
 
-// Variables for the duration and the distance
+// Define os pinos dos LEDs
+const int LED_RED1 = 2;
+const int LED_GREEN = 3;
+const int LED_RED2 = 4;
+
+// Define o pino do buzzer e seu estado inicial
+int buzzpin = 7;
+int buzzState = LOW;
+
+// Variáveis para armazenar a duração e a distância
 long duration;
 int distance;
-Servo myServo; // Creates a servo object for controlling the servo motor
+Servo myServo;  // Cria um objeto servo para controlar o motor servo
+
+// Variáveis de intervalo de tempo
+unsigned long previousMillis = 0;
+const long intervalFar = 250;
+const long intervalClose = 50;
+
 void setup() {
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-  Serial.begin(9600);
-  myServo.attach(5); // Defines on which pin is the servo motor attached
+  pinMode(trigPin, OUTPUT);    // Define o pino trigPin como Saída
+  pinMode(echoPin, INPUT);     // Define o pino echoPin como Entrada
+  Serial.begin(9600);          // Inicializa a comunicação serial
+  myServo.attach(12);          // Define o pino ao qual o motor servo está conectado
+  pinMode(LED_RED1, OUTPUT);   // Define o pino ao qual o LED vermelho está conectado.
+  pinMode(LED_RED2, OUTPUT);   // Define o pino dois ao qual o LED vermelho está conectado.
+  pinMode(LED_GREEN, OUTPUT);  // Define o pino ao qual o LED verde está conectado.
+  pinMode(buzzpin, OUTPUT);    // Define o pino do buzzer como Saída
 }
+
 void loop() {
-  // rotates the servo motor from 15 to 165 degrees
-  for(int i=0;i<=180;i++){  
-  myServo.write(i);
-  delay(30);
-  distance = calculateDistance();// Calls a function for calculating the distance measured by the Ultrasonic sensor for each degree
-  
-  Serial.print(i); // Sends the current degree into the Serial Port
-  Serial.print(","); // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
-  Serial.print(distance); // Sends the distance value into the Serial Port
-  Serial.print("."); // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
+  // Varredura do motor servo de 0 a 180 graus
+  for (int i = 0; i <= 180; i++) {
+    myServo.write(i);
+    delay(30);
+    distance = calculateDistance();
+
+    // Condições de acionamento dos LEDs e buzzer com base na distância
+    if (distance <= 40 && distance > 20) {
+      unsigned long currentMillis = millis();
+      if (currentMillis - previousMillis >= intervalFar) {
+        previousMillis = currentMillis;
+        buzzState = !buzzState;  // Alterna o estado do buzzer e LED vermelho
+        digitalWrite(buzzpin, buzzState);
+        digitalWrite(LED_RED1, buzzState);
+        digitalWrite(LED_GREEN, HIGH);
+        digitalWrite(LED_RED2, LOW); // Apaga o LED vermelho 2 se não está na faixa mais próxima
+      }
+    } else if (distance <= 20 && distance > 0) {
+      unsigned long currentMillis = millis();
+      if (currentMillis - previousMillis >= intervalClose) {
+        previousMillis = currentMillis;
+        buzzState = !buzzState;  // Alterna o estado do buzzer e LED vermelho
+        digitalWrite(buzzpin, buzzState);
+        digitalWrite(LED_RED1, buzzState);
+        digitalWrite(LED_RED2, buzzState); // Acende o LED vermelho 2 para distância < 20 cm
+        digitalWrite(LED_GREEN, HIGH);
+      }
+    } else if (distance > 40) {
+      digitalWrite(buzzpin, LOW);
+      digitalWrite(LED_GREEN, LOW);
+      digitalWrite(LED_RED1, HIGH);
+      digitalWrite(LED_RED2, LOW); // Apaga o LED vermelho 2 se não está na faixa de < 20 cm
+    }
+
+    Serial.print(i);
+    Serial.print(",");
+    Serial.print(distance);
+    Serial.print(".");
   }
-  // Repeats the previous lines from 165 to 15 degrees
-  for(int i=180;i>=0;i--){  
-  myServo.write(i);
-  delay(30);
-  distance = calculateDistance();
-  Serial.print(i);
-  Serial.print(",");
-  Serial.print(distance);
-  Serial.print(".");
+
+  // Varredura da esquerda para a direita
+  for (int i = 180; i >= 0; i--) {
+    myServo.write(i);
+    delay(30);
+    distance = calculateDistance();
+
+    // Condições de acionamento dos LEDs e buzzer com base na distância
+    if (distance <= 40 && distance > 20) {
+      unsigned long currentMillis = millis();
+      if (currentMillis - previousMillis >= intervalFar) {
+        previousMillis = currentMillis;
+        buzzState = !buzzState;
+        digitalWrite(buzzpin, buzzState);
+        digitalWrite(LED_RED1, buzzState);
+        digitalWrite(LED_GREEN, HIGH);
+        digitalWrite(LED_RED2, LOW); // Apaga o LED vermelho 2 se não está na faixa mais próxima
+      }
+    } else if (distance <= 20 && distance > 0) {
+      unsigned long currentMillis = millis();
+      if (currentMillis - previousMillis >= intervalClose) {
+        previousMillis = currentMillis;
+        buzzState = !buzzState;
+        digitalWrite(buzzpin, buzzState);
+        digitalWrite(LED_RED1, buzzState);
+        digitalWrite(LED_RED2, buzzState); // Acende o LED vermelho 2 para distância < 20 cm
+        digitalWrite(LED_GREEN, HIGH);
+      }
+    } else if (distance > 40) {
+      digitalWrite(buzzpin, LOW);
+      digitalWrite(LED_GREEN, LOW);
+      digitalWrite(LED_RED1, HIGH);
+      digitalWrite(LED_RED2, LOW); // Apaga o LED vermelho 2 se não está na faixa de < 20 cm
+    }
+
+    Serial.print(i);
+    Serial.print(",");
+    Serial.print(distance);
+    Serial.print(".");
   }
 }
-// Function for calculating the distance measured by the Ultrasonic sensor
-int calculateDistance(){ 
-  
-  digitalWrite(trigPin, LOW); 
+
+// Função para calcular a distância medida pelo Sensor Ultrassônico
+int calculateDistance() {
+  digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
-  // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin, HIGH); 
+  digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH); // Reads the echoPin, returns the sound wave travel time in microseconds
-  distance= duration*0.034/2;
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2;
   return distance;
 }
